61d3b2a7cf9492f8974735e674ff368cf6f6b367
Felix Kyros Ang <fkyros@Felixs-MacBook-Pro.local>
2019-02-10 11:59:59 -0500

    Cleaning up core code, updating for non-Linux systems

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # hask-deduplicator
-Attempt to create a parallel duplicate identifier in Haskell.
+My attempt to create a parallel duplicate identifier in Haskell.
+This project was inspired by a need to speed up the detection of duplicate contact information in a database, where users were intentionally evading the duplicate rules in a major CRM.
 
-No longer runs out of memory!
+No it longer runs out of memory!
 
 ## Usage
 ### Build
+Exposing the Haskell RTS to the command line
 `stack ghc -- -rtsopts -O2 -threaded leven.hs`
 
-### Execute
+### Execute with RTS args
 `./leven +RTS -N`
+
+### Build with multithreading on default
+`stack ghc -- --with-rtsopts="-N" -O2 -threaded leven.hs`
+
+### Execute
+`./leven`
--- leven.hs
+++ leven.hs
@@ -1,34 +1,20 @@
-
 module Main where
 
 import qualified Data.Text as T
 import Data.Text.Metrics
-import Data.Ratio
 import Control.Parallel.Strategies
 import System.CPU (logicalCores, getCPUs)
+import System.Info (os)
 
 wrds :: IO [String]
-wrds = readFile "cracklib-smaller" >>= return . words 
-        {-
-twrds :: IO [T.Text]
-twrds = T.words <$> fmap T.pack wrds 
--}
-first :: (a, b, c) -> a
-first (a, _, _) = a
+wrds = readFile "smallwords" >>= return . words 
 
-second :: (a, b, c) -> b
-second (_, b, _) = b
-
-third :: (a, b, c) -> c
-third (_, _, c) = c
-
-pairme :: [String] -> [(String, String)]
-pairme x = [(x1, x2) | x1 <- x, x2 <- x, x1 /= x2]
-
-similarityCalc :: (String, String) -> (String, String, Ratio Int)
-similarityCalc (x, y) = (x, y, levenshteinNorm (T.pack x) (T.pack y))
-
-
+cores :: IO Int
+cores = case os of
+          "darwin" -> return 4
+          otherwise -> do
+                        cpu <- getCPUs
+                        return $ logicalCores cpu
 
 calcLev :: [String] -> String -> [(String, String)]
 calcLev base word = go base [] where
@@ -40,7 +26,6 @@ calcLev base word = go base [] where
 main :: IO ()
 main = do
         allWords <- wrds
-        cpu <- getCPUs
-        let z = map (calcLev allWords) allWords `using` (parListChunk (length allWords `div` logicalCores cpu)) rdeepseq
+        coreNum <- cores
+        let z = map (calcLev allWords) allWords `using` (parListChunk (length allWords `div` coreNum)) rdeepseq
          in putStrLn $ show (concat z)
-                {- putStrLn $ show (filter (\x -> (first x /= second x) && (third x > 0.85)) $ similarityCalc allWords)-}
