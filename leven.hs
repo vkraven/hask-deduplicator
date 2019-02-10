49b249ac361d2f462cf33b6d57b21b90d25e0a73
vkraven <felixk@vkraven.com>
2019-02-09 20:29:14 -0500

    No longer runs out of memory, and performance increased!

--- leven.hs
+++ leven.hs
@@ -5,13 +5,14 @@ import qualified Data.Text as T
 import Data.Text.Metrics
 import Data.Ratio
 import Control.Parallel.Strategies
+import System.CPU (logicalCores, getCPUs)
 
-wrds :: IO String
-wrds = readFile "/usr/share/dict/cracklib-small" 
-
+wrds :: IO [String]
+wrds = readFile "cracklib-smaller" >>= return . words 
+        {-
 twrds :: IO [T.Text]
-twrds = T.words <$> fmap T.pack wrds
-
+twrds = T.words <$> fmap T.pack wrds 
+-}
 first :: (a, b, c) -> a
 first (a, _, _) = a
 
@@ -21,17 +22,25 @@ second (_, b, _) = b
 third :: (a, b, c) -> c
 third (_, _, c) = c
 
-pairme :: [T.Text] -> [(T.Text, T.Text)]
+pairme :: [String] -> [(String, String)]
 pairme x = [(x1, x2) | x1 <- x, x2 <- x, x1 /= x2]
 
-similarityCalc :: (T.Text, T.Text) -> (String, String, Ratio Int)
-similarityCalc (x, y) = (T.unpack x, T.unpack y, levenshteinNorm x y)
+similarityCalc :: (String, String) -> (String, String, Ratio Int)
+similarityCalc (x, y) = (x, y, levenshteinNorm (T.pack x) (T.pack y))
+
+
+
+calcLev :: [String] -> String -> [(String, String)]
+calcLev base word = go base [] where
+                       go (x:xs) res
+                         | xs == [] = if (x /= word) && (levenshteinNorm (T.pack x) (T.pack word) > 0.85) then [(word, x)] <> res else res
+                         | otherwise = go xs (if (x /= word) && (levenshteinNorm (T.pack x) (T.pack word) > 0.85) then [(word, x)] <> res else res)
 
 
 main :: IO ()
 main = do
-        allWords <- twrds
-        let y = pairme allWords
-            z = map similarityCalc y `using` (parListChunk 1000) rdeepseq
-         in putStrLn $ show (filter (\x -> third x > 0.85) z)
+        allWords <- wrds
+        cpu <- getCPUs
+        let z = map (calcLev allWords) allWords `using` (parListChunk (length allWords `div` logicalCores cpu)) rdeepseq
+         in putStrLn $ show (concat z)
                 {- putStrLn $ show (filter (\x -> (first x /= second x) && (third x > 0.85)) $ similarityCalc allWords)-}
